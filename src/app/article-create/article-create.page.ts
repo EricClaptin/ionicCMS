e73c46aecaab1054756bf581d5d06932bdfe3b4e
Eric Carlson <ericc254@yahoo.com>
2020-04-23 15:48:06 -0500

    create works

--- src/app/article-create/article-create.page.ts
+++ src/app/article-create/article-create.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { ArticlesService } from '../articles.service';
+import { Article } from '../article.model'; 
 
 @Component({
   selector: 'app-article-create',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleCreatePage implements OnInit {
 
-  constructor() { }
+  article:Article = new Article();
+  errors: Array<any> = [];
+  errorMessage: string;
 
-  ngOnInit() {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private articlesService: ArticlesService
+  ) { }
+
+  ngOnInit(): void{}
+
+  response(response:any): void{
+
+    if(response.success===false){
+      this.errors = response.error.errors;
+      this.errorMessage = response.error._message;
+    }
+
+    if(response.success===true){
+      this.router.navigate(['/articles']);
+    }
+  }
+
+  onSubmit(): void{
+    this.articlesService.createArticle(this.article).subscribe(
+      (response:any) => {
+        this.response(response);
+      }
+    );
   }
 
 }
--- src/app/article/article.page.ts
+++ src/app/article/article.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ArticlesService } from '../articles.service';
+import { Article } from '../article.model'; 
 
 @Component({
   selector: 'app-article',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlePage implements OnInit {
 
-  constructor() { }
+  article: Article;
+  
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private articlesService: ArticlesService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params=>{
+      this.getArticle(params['articleId']);
+    });
   }
 
+  getArticle(id:string):void {
+    this.articlesService.getArticle(id).subscribe(
+      (response:any)=>{
+        this.article = response.article;
+      }
+    );
+  }
 }
